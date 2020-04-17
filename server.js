6d22eea84223dec8fa472dc2b4e5fe8877adc56a
waritthonten <waritthon_auttama@cmu.ac.th>
2020-04-17 21:27:50 +0700

    Initial commit

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use('/exercises', exercisesRouter);
 
 if(process.env.NODE_ENV==='production'){
   app.use(express.static('client/build'));
+  
   app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'client','build','index.html'));
   });
